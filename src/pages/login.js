6edb4ca4573df5c024c1c08719b836840b95d527
kapil551 <59477738+kapil551@users.noreply.github.com>
2021-12-27 15:18:42 +0530

    Login Page - continue building the login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -20,9 +20,80 @@ export default function Login() {
         }, []
     );
 
+    const handleLogin = async (event) => {
+        event.preventDefault();
+    
+        try {
+          await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
+          navigate(ROUTES.DASHBOARD, { replace: true }); // navigate to dashboard on successfull login
+        } catch (error) {
+          setEmailAddress('');
+          setPassword('');
+          setError(error.message);
+        }
+      };
+
     return (
-        <div className='bg-red-500'>
-            I am a login page
+        
+        <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+          
+          <div className="flex w-3/5">
+            <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram app" />
+          </div>
+
+          <div className="flex flex-col w-2/5">
+            
+            <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
+              
+              <h1 className="flex justify-center w-full">
+                <img src="/images/logo.png" alt="socialmedia-logo" className="mt-2 w-6/12 mb-4" />
+              </h1>
+    
+              {error && <p className="mb-4 text-xs text-red-500">{error}</p>}
+    
+              <form onSubmit={handleLogin} method="POST">
+                
+                <input
+                  aria-label="Enter your email address"
+                  type="text"
+                  placeholder="Email address"
+                  className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                  onChange={({ target }) => setEmailAddress(target.value)}
+                  value={emailAddress}
+                />
+
+                <input
+                  aria-label="Enter your password"
+                  type="password"
+                  placeholder="Password"
+                  className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                  onChange={({ target }) => setPassword(target.value)}
+                  value={password}
+                />
+
+                <button
+                  disabled={isInvalid}
+                  type="submit"
+                  className={`bg-blue-medium text-white w-full rounded h-8 font-bold ${isInvalid && 'opacity-50'}`}
+                >
+                  Login
+                </button>
+
+              </form>
+
+            </div>
+
+            <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
+              <p className="text-sm">
+                Do not have an account?{` `}
+                <Link to={ROUTES.SIGN_UP} className="font-bold text-blue-medium">
+                  Sign up
+                </Link>
+              </p>
+            </div>
+
+          </div>
+
         </div>
-    )
+    );
 }
